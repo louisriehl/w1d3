40feae83b27de3408d3e2f868933e2b7f3c2f30e
Louis Riehl <louisriehl@gmail.com>
2018-09-27 19:39:50 +0000

    modular functionality for printWhat(), can print followers and followed by

--- social-analyzer.js
+++ social-analyzer.js
@@ -33,28 +33,96 @@ var data = {
 
 };
 
-  function returnUsersOnId (idArray) // when given an array of ids, returns the users they correspond to
+function returnUsersOnId (idArray) // when given an array of ids, returns the users they correspond to
+{
+  var string = "";
+  // console.log(idArray);
+  // console.log(this.f01.name);
+  for (var i = 0; i < idArray.length; i++)
   {
-    var string = "";
-    // console.log(idArray);
-    // console.log(this.f01.name);
-    for (var i = 0; i < idArray.length; i++)
-    {
-      var id = idArray[i];
-      string += data[id]['name'] + " ";
-    }
-    return string;
+    var id = idArray[i];
+    string += data[id]['name'] + " ";
+  }
+  return string;
+}
+
+function printWhat(string)
+{
+  var tag;
+  var attribute;
+
+  if (string === 'following')
+  {
+    tag = " follows : ";
+    attribute = 'follows';
+  }
+  else if (string === 'followers')
+  {
+    tag = " is followed by : ";
+    attribute = 'followedBy';
   }
 
-  function printAllFollowing() // print who each user is following
+  for (var users in data)
   {
-    for (var users in data)
+    var response = '';
+    response += data[users]['name'] + tag;
+    response += returnUsersOnId(data[users][attribute]);
+    console.log(response);
+  }
+}
+
+function printAllFollowing() // print who each user is following
+{
+  for (var users in data)
+  {
+    var following = "";
+    following += data[users]['name'] + ' follows : ';
+    following += returnUsersOnId(data[users]['follows']);
+    console.log(following);
+  }
+}
+
+function printAllFollowers() // print who each user is following
+{
+  for (var users in data)
+  {
+    var following = "";
+    following += data[users]['name'] + ' is followed by : ';
+    following += returnUsersOnId(data[users]['followedBy']);
+    console.log(following);
+  }
+}
+function setFollowing()
+{
+
+  function setter() //adds followedBy property to each object
+  {
+    for (var each in data)
     {
-      var following = "";
-      following += data[users]['name'] + ' follows : ';
-      following += returnUsersOnId(data[users]['follows']);
-      console.log(following);
+      data[each]['followedBy'] = [];
     }
   }
 
-  printAllFollowing();
+  setter();
+
+  //get the follows array of each user
+  for (var user in data)
+  {
+    var followsArray = data[user]['follows'];
+    // console.log(data[user]['name'], 'follows', followsArray);
+    for (var i = 0; i < followsArray.length; i++)
+    {
+      data[followsArray[i]]['followedBy'].push(user);
+      // console.log(data[followsArray[i]]['followedBy']);
+    }
+  }
+
+}
+
+// printAllFollowing();
+
+setFollowing();
+printWhat('followers');
+// printAllFollowers();
+// printAllFollowing();
+// console.log(data);
